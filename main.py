cf9f796e75e67f11e7c1866691a898f357fdfab6
Isabela.leite <isabela.leeite@hotmail.com>
2024-06-12 17:14:11 -0300

    exercicios da aula 02 do Bootcamp de Python

---
+++ main.py
@@ -0,0 +1,12 @@
+# x = -3
+
+# if x <= 2:
+#     print('É menor do que zero')
+# elif x <= 00:
+#     print('É menor do que dois')
+# else:
+#     print('More')
+    
+    
+for i in range (1,5):
+    print(i)
